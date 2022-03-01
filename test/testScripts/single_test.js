9bf3f91d76db3b9aa58d683a667965cc7543ac3b
Shreyansh <t-shreyansh.ayanger@ocltp.com>
2022-03-01 18:16:53 +0530

    abs

--- test/testScripts/single_test.js
+++ test/testScripts/single_test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('IndexArray', function() {
   describe('#checkIndex negative()', function() {
     it('the function should return -1 when the value is not present', function(){
-      assert.equal(-1, [4,5,6].indexOf(7));
+      assert.equal(1, [4,5,6].indexOf(5));
     });
   });
 });
